a656b006c0f3aa48dbf767446fc8b51fd79cc746
David Fernando Castañeda Hincapie <160748912+DavidFCH059@users.noreply.github.com>
2024-11-29 14:16:07 -0500

    Update PedidoRoutes.js
    ruta nueva

--- Routes/PedidoRoutes.js
+++ Routes/PedidoRoutes.js
@@ -1,15 +1,20 @@
-// Routes/PedidoRoutes.js
 const express = require('express');
 const router = express.Router();
 const pedidoController = require('../Controllers/PedidoController');
 
 // Ruta para obtener todos los pedidos
-router.get('/', pedidoController.obtenerPedidos); // Cambia a 'obtenerPedidos'
+router.get('/', pedidoController.obtenerPedidos);
 
 // Ruta para crear un nuevo pedido
-router.post('/', pedidoController.crearPedido); // Cambia a 'crearPedido'
+router.post('/', pedidoController.crearPedido);
 
-// Ruta para actualizar un pedido por su ID
+// Ruta para actualizar el estado del pedido por su ID
 router.put('/:id', pedidoController.actualizarEstadoPedido);
+
+// Ruta para actualizar todo el pedido por su ID
+router.put('/actualizar/:id', pedidoController.actualizarPedidoCompleto);
+
+// Ruta para eliminar un pedido por su ID
+router.delete('/:id', pedidoController.eliminarPedido);
 
 module.exports = router;
