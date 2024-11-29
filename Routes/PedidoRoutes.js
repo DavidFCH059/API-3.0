eae411afb429fa475c51b372694f972abd3579f3
David Fernando Castañeda Hincapie <160748912+DavidFCH059@users.noreply.github.com>
2024-11-29 16:10:44 -0500

    Update PedidoRoutes.js
    Rutas Corregidas

--- Routes/PedidoRoutes.js
+++ Routes/PedidoRoutes.js
@@ -11,9 +11,6 @@ router.post('/', pedidoController.crearPedido);
 // Ruta para actualizar el estado del pedido por su ID
 router.put('/:id', pedidoController.actualizarEstadoPedido);
 
-// Ruta para actualizar todo el pedido por su ID
-router.put('/actualizar/:id', pedidoController.actualizarPedidoCompleto);
-
 // Ruta para eliminar un pedido por su ID
 router.delete('/:id', pedidoController.eliminarPedido);
 
