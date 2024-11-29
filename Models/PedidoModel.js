5c178efcef0e7c72a7e5233db4cbcd9ba9d5d132
David Fernando Castañeda Hincapie <160748912+DavidFCH059@users.noreply.github.com>
2024-11-29 16:02:09 -0500

    Update PedidoModel.js
    Nuevo Modelo Pedido

--- Models/PedidoModel.js
+++ Models/PedidoModel.js
@@ -1,18 +1,19 @@
-// Models/Pedido.js
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PedidoSchema = new Schema({
-    cliente: { type: Schema.Types.ObjectId, ref: 'Cliente', required: true },
+    nombreCliente: { type: String, required: true },
+    correoCliente: { type: String, required: true },
+    telefonoCliente: { type: String, required: true },
     productos: [{
-        producto: { type: Schema.Types.ObjectId, ref: 'Product', required: true }, // Asegúrate de que el nombre sea 'Product'
+        nombreProducto: { type: String, required: true },
         cantidad: { type: Number, required: true },
-        subtotal: { type: Number, required: true }
+        precioUnitario: { type: Number, required: true },
+        subtotal: { type: Number, required: true }  // El subtotal de cada producto (cantidad * precio)
     }],
-    subtotal: { type: Number, required: true },
-    iva: { type: Number, required: true },
-    total: { type: Number, required: true },
-    metodoPago: { type: String, enum: ['efectivo', 'transferencia'], required: true },
+    subtotal: { type: Number, required: true },  // El subtotal total de los productos
+    total: { type: Number, required: true },  // El total (subtotal)
+    metodoPago: { type: String, enum: ['efectivo', 'tarjeta', 'transferencia'], required: true },
     direccionEnvio: { type: String, required: true },
     estado: { type: String, enum: ['pendiente', 'enviado', 'entregado', 'cancelado'], default: 'pendiente' },
     fechaPedido: { type: Date, default: Date.now }
