560c544b8063e9c44c5ba3247abd3a1be8fabddd
Juanf3r <160983422+Juan-f3rnand0@users.noreply.github.com>
2024-11-30 16:52:07 -0500

    Update UsuarioModel.js
    Nuevo modelo usuario

--- Models/UsuarioModel.js
+++ Models/UsuarioModel.js
@@ -1,36 +1,12 @@
 const mongoose = require('mongoose');
- 
+
 const esquemaUsuario = new mongoose.Schema({
-  nombre: {
-    type: String,
-    required: true
-  },
-  apellido: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  contrasena: {
-    type: String,
-    required: true
-  },
-  rol: {
-    type: String,
-    enum: ['admin', 'usuario', 'invitado'],
-    default: 'usuario'
-  },
-  estado: {
-    type: String,
-    enum: ['activo', 'inactivo'],
-    default: 'activo'
-  },
-  tokenRecuperacion: String,
-  expiracionTokenRecuperacion: Date
+  nombre: { type: String, required: true },
+  apellido: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  contrasena: { type: String, required: true },
+  rol: { type: String, enum: ['admin', 'usuario', 'invitado'], default: 'usuario' },
+  estado: { type: String, enum: ['activo', 'inactivo'], default: 'activo' }
 });
- 
+
 module.exports = mongoose.model('Usuario', esquemaUsuario);
- 
