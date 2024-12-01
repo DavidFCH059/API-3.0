3a4c463f5dae158524e5ac6afff082f82aa37d3c
Juanf3r <160983422+Juan-f3rnand0@users.noreply.github.com>
2024-12-01 15:03:05 -0500

    Update DevolucionModel.js
    Nuevo modelo devolución

--- Models/DevolucionModel.js
+++ Models/DevolucionModel.js
@@ -1,40 +1,14 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const DevolucionSchema = new Schema({
-  factura: {
-    type: String,
-    required: true
-  },
-  nombreProducto: {
-    type: String,
-    required: true
-  },
-  fechaDevolucion: {
-    type: Date,
-    default: Date.now
-  },
-  cantidad: {
-    type: Number,
-    required: true
-  },
-  precio: {
-    type: Number,
-    required: true
-  },
-  montoTotal: {
-    type: Number,
-    required: true
-  },
-  motivo: {
-    type: String,
-    required: true
-  },
-  estadoEnvio: {
-    type: String,
-    enum: ['En proceso', 'Cerrado', 'Solicitado'],
-    default: 'En proceso'
-  },
+const DevolucionSchema = new mongoose.Schema({
+  factura: { type: String, required: true },
+  nombreProducto: { type: String, required: true },
+  fechaDevolucion: { type: Date, default: Date.now },
+  cantidad: { type: Number, required: true },
+  precio: { type: Number, required: true },
+  montoTotal: { type: Number, required: true },
+  motivo: { type: String, required: true },
+  estadoEnvio: { type: String, enum: ['En proceso', 'Cerrado', 'Solicitado'], default: 'En proceso' },
   estadoPedido: {
     type: String,
     enum: [
