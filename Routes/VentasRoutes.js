e983e22727c898a7cdd321b43823e479ba526054
Andreacetre <160748638+Andreacetre@users.noreply.github.com>
2024-12-02 10:15:45 -0500

    Update VentasRoutes.js
    cambio ruta ventas

--- Routes/VentasRoutes.js
+++ Routes/VentasRoutes.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 const VentaController = require('../Controllers/VentaController');
 
-router.post('/', VentaController.crearVenta); // Crear una venta
-router.get('/', VentaController.obtenerVentas); // Obtener todas las ventas
-router.get('/:id', VentaController.obtenerVentaPorId); // Obtener venta por ID
-router.put('/:id', VentaController.actualizarVenta); // Actualizar venta
-router.delete('/:id', VentaController.anularVenta); // Anular venta
+// Crear una venta
+router.post('/', VentaController.crearVenta);
+
+// Obtener todas las ventas
+router.get('/', VentaController.obtenerVentas);
+
+// Obtener venta por ID
+router.get('/:id', VentaController.obtenerVentaPorId);
+
+// Actualizar venta
+router.put('/:id', VentaController.actualizarVenta);
+
+// Anular venta
+router.delete('/:id', VentaController.anularVenta);
 
 module.exports = router;
+
