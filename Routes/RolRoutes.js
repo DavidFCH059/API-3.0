478a41ae0ff7b9af114d021808237e03e4680876
samuelmolina1212 <samuelmolinaochoa@gmail.com>
2024-12-11 21:52:53 -0500

    Update RolRoutes.js
    actualizacion rol routes

--- Routes/RolRoutes.js
+++ Routes/RolRoutes.js
@@ -1,20 +1,21 @@
 const express = require('express');
+const rolController = require('../Controllers/RolController');
 const router = express.Router();
-const rolController = require('../Controllers/rolController');
 
-// Crear un nuevo rol
+// Crear rol
 router.post('/', rolController.crearRol);
 
 // Obtener todos los roles
 router.get('/', rolController.obtenerRoles);
 
-// Obtener un rol por ID
+// Obtener rol por ID
 router.get('/:id', rolController.obtenerRolPorId);
 
-// Actualizar un rol
+// Actualizar rol
 router.put('/:id', rolController.actualizarRol);
 
-// Eliminar un rol
+// Eliminar rol
 router.delete('/:id', rolController.eliminarRol);
 
 module.exports = router;
+
